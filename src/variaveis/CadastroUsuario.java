73560f62bff023fcc23df99e87ef55eaf6f6ac19
David Reis <davidluccas132@gmail.com>
2026-07-17 12:19:59 -0300

    add commit

--- src/variaveis/CadastroUsuario.java
+++ src/variaveis/CadastroUsuario.java
@@ -1,4 +1,18 @@
 package variaveis;
 
 public class CadastroUsuario {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+       String nome = scanner.nextLine();
+
+        System.out.println("Digite sua idade");
+               int idade = scanner.nextInt();
+
+
+               scanner.close();
+
+        System.out.println("Olá " + nome + "," + "voce tem " + idade + " anos!");
+    }
 }
